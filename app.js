5a58a002349c65cb4ac6c26eb547daba2bee5862
Jimen120 <enrique.jim11@gmail.com>
2020-12-13 15:17:11 -0800

    commit 3:17pm 12/13/2020 website deploy test

---
+++ app.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDIQQAZ1RTaKJYVpgHP_lSnBq7U7kXPKIY",
+    authDomain: "cs441-software-engineering.firebaseapp.com",
+    projectId: "cs441-software-engineering",
+    storageBucket: "cs441-software-engineering.appspot.com",
+    messagingSenderId: "39038256631",
+    appId: "1:39038256631:web:af0b5487b08ccf738d9af2",
+    measurementId: "G-ZYM0SJT64T"
+  };
+
+//   new code from YT vid
+firebaseConfig.initializeApp(config);
