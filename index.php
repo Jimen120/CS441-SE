40ca6ac34a55e135b42623feeb990b5fcb1061e8
Jimen120 <enrique.jim11@gmail.com>
2020-12-14 10:35:11 -0800

    Final commit 10:35am 12/14/20

--- index.php
+++ index.php
@@ -1,44 +1,53 @@
 <?php
-include 'db_connection.php';
-
-$conn = OpenCon();
 
 
-// $stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
-// 		$stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
-// 		$execval = $stmt->execute();
-// 		echo $execval;
-// 		echo "Registration successfully...";
-// 		$stmt->close();
-// 		$conn->close();
+if(!empty($firstname) || !empty($lastname) || !empty($email) || !empty($password))
+{
+$host = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbname = "test";
 
-// echo "Connected Successfully";
+//creating the connection
+$conn = new mysqli($host, $dbUsername);
 
-$firstName = $_POST['firstName'];
-$lastName = $_POST['lastName'];
-$gender = $_POST['gender'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$number = $_POST['number'];
+if(mysqli_connect_error())
+{
+	die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+}
+else{
+	$SELECT = "SELECT email From users Where email = ? Limit 1";
+	$INSERT = "INSERT Into users (firstname, lastname, email, password) values (?, ?, ?, ?)";
 
-// Database connection
-$conn = new mysqli('localhost','root','','test');
-if($conn->connect_error){
-    echo "$conn->connect_error";
-    die("Connection Failed : ". $conn->connect_error);
-} else {
-    $stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
-    $execval = $stmt->execute();
-    echo $execval;
-    echo "Registration successfully...";
-    $stmt->close();
-    $conn->close();
+	//prepare statement
+	$stmt = $conn->prepare($SELECT);
+	$stmt->bind_param("s", $email);
+	$stmt->execute();
+	$stmt->bind_result($email);
+	$stmt->store_result();
+	$rnum = $stmt->num_rows;
+
+	if($rnum==0)
+	{
+		$stmt->close();
+
+		$stmt = $conn->prepare($INSERT);
+		$stmt->bind_param("ssss",$firstname, $lastname, $email, $password);
+		$stmt->execute();
+		echo "New record has been updated successfully";
+	}
+	else{
+		echo "someone already registered using this email";
+	}
+	$stmt->close();
+	$conn->close();
 }
 
+}
+else{
+	echo "Data successfully inserted";
+	die();
+}
 
-
-
-CloseCon($conn);
 
 ?>
