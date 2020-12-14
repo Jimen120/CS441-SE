ab17bd36073a14d781363375932c6911242e5ee6
Jimen120 <enrique.jim11@gmail.com>
2020-12-13 21:07:47 -0800

    commit 9:07pm

--- connect.php
+++ connect.php
@@ -1,4 +1,3 @@
-
 <?php
 	$firstName = $_POST['firstName'];
 	$lastName = $_POST['lastName'];
@@ -8,7 +7,7 @@
 	$number = $_POST['number'];
 
 	// Database connection
-	$conn = new mysqli('localhost','root','','wishlistdb');
+	$conn = new mysqli('localhost','root','','test');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
